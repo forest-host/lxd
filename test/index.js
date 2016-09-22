e56abd21890f773a6566eccef5de571cbe2c2941
Johan Schuijt <johan@publicpixels.nl>
2016-09-22 11:24:54 +0200

    add tests

---
+++ test/index.js
@@ -0,0 +1,32 @@
+//import assert from 'assert';
+
+describe('LXC Module', () => {
+	// TODO set / unset config?
+	describe('create', () => {
+		it('Creates container');
+	});
+
+	describe('destroy', () => {
+		it('Destroys container');
+	});
+
+	describe('copy_to', () => {
+		it('Copies data from host to container');
+	});
+
+	describe('copy_from', () => {
+		it('Copies data from container to host');
+	});
+
+	describe('mount', () => {
+		it('Mounts data volume on container');
+	});
+
+	describe('unmount', () => {
+		it('Unmounts data volume from container');
+	});
+
+	describe('execute', () => {
+		it('Executes command in container');
+	});
+});
