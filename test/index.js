154f291fa2e033331279f5a1e8c5a10a43ea6141
Johan Schuijt <johan@publicpixels.nl>
2016-10-05 13:30:45 +0200

    change tests

--- test/index.js
+++ test/index.js
@@ -4,28 +4,35 @@ import Promise from 'bluebird';
 import fs from 'fs';
 import path from 'path';
 import chai from 'chai';
+import exec from 'utilities';
 chai.should();
 
 import lxc from '../lib';
 
 var stat = Promise.promisify(fs.stat);
 
-function clean_output(output) {
+var name = 'test';
+var image = 'test';
+var user = 'forest';
+var directory = 'dist';
+var container_path = '/home/' + user + '/' + directory;
+
+function parse_output(output) {
 	return output.stdout.map(line => line.toString().replace('\n', ''));
 }
 
+function filter_containers(list) {
+	return list.filter(container => container.name == name);
+}
+
 describe('LXC Module', () => {
-	var name = 'test';
-	var image = 'test';
-	var user = 'forest';
-	var directory = 'dist';
-	var container_path = '/home/' + user + '/' + directory;
 
 	describe('create', () => {
 		it('Creates container', function() {
 			this.timeout(30000);
 			return lxc.create(image, name)
 				.then(() => lxc.list())
+				.then(filter_containers)
 				.then(list => list.should.have.length(1));
 		});
 	});
@@ -33,6 +40,7 @@ describe('LXC Module', () => {
 	describe('list', () => {
 		it('Lists containers', () => {
 			return lxc.list()
+				.then(filter_containers)
 				.then(list => {
 					list.should.have.length(1);
 					list[0].name.should.equal(name);
@@ -48,7 +56,7 @@ describe('LXC Module', () => {
 			};
 
 			return lxc.execute(name, 'hostname', options)
-				.then(clean_output)
+				.then(parse_output)
 				.then(lines => lines[0])
 				.then(output => output.should.equal(name))
 		});
@@ -66,19 +74,22 @@ describe('LXC Module', () => {
 		it('Copies data from host to container', () => {
 			return lxc.copy_to(name, host_path, container_path)
 				.then(() => lxc.execute(name, 'ls ' + path.dirname(container_path)))
-				.then(clean_output)
+				.then(parse_output)
 				.then(lines => lines[0])
 				.then(line => line.should.equal(directory));
 		});
 	});
 
 	describe('copy_from', () => {
-		var host_path = '/tmp/dist';
+		var host_path = '/tmp/testing';
 
 		it('Copies data from container to host', () => {
 			return lxc.copy_from(name, container_path, host_path)
-				.then(() => stat(host_path))
-				.then(stats => stats.should.have.property('size'));
+				.then(() => exec('ls', [host_path]))
+				.then(parse_output)
+				.then(output => {
+					output.should.contain('index.js');
+				});
 		});
 	});
 
@@ -87,6 +98,7 @@ describe('LXC Module', () => {
 			this.timeout(5000);
 			return lxc.destroy(name)
 				.then(() => lxc.list())
+				.then(filter_containers)
 				.then(list => list.should.have.length(0));
 		});
 	});
