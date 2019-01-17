abc3211b015b91ddf9fe091a5c3dd59a929c6664
Johan Schuijt <johan@publicpixels.nl>
2019-01-17 16:59:45 +0100

    rebuild

--- dist/pool.js
+++ dist/pool.js
@@ -16,6 +16,9 @@ function Pool(client, name) {
 	this.url = '/storage-pools/' + this.name + '/volumes';
 }
 
+/**
+ * List storage volumes in this pool
+ */
 Pool.prototype.list = function () {
 	var _this = this;
 
@@ -32,12 +35,24 @@ Pool.prototype.list = function () {
 	});
 };
 
-Pool.prototype.create_volume = function (name) {
-	return this.client.run_sync_operation('POST', this.url, {
+/**
+ * Create a volume in this storage pool, possibly on cluster target
+ */
+Pool.prototype.create_volume = function (name, target) {
+	// Build query string
+	var qs = {};
+	if (typeof target !== 'undefined') {
+		qs.target = target;
+	};
+
+	// Volume config
+	var config = {
 		config: {},
 		name,
 		type: 'custom'
-	});
+	};
+
+	return this.client.run_sync_operation('POST', this.url, config, qs);
 };
 
 Pool.prototype.destroy_volume = function (name) {
